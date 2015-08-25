6e03e40ead4ce030f160823266857f6c613eece2
funboxxx <funboxxx@wp.pl>
2015-08-25 20:16:21 +0200

    2.0.0.6

--- Funbox Rengar/Program.cs
+++ Funbox Rengar/Program.cs
@@ -34,8 +34,7 @@ namespace Rengar
             var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
             TargetSelector.AddToMenu(targetSelectorMenu);
             _config.AddSubMenu(targetSelectorMenu);
-            _config.AddItem(new MenuItem("emode", "E mode").SetValue(false));
-            _config.AddItem(new MenuItem("mmode", "Oneshot mode").SetValue(false));
+            _config.AddItem(new MenuItem("ComboMode", "Combo Mode").SetValue(new StringList(new[]{"Empowered Q", "Empowered E"})));
             _config.AddItem(new MenuItem("autoheal", "%hp autoheal").SetValue(new Slider(35, 100, 0)));
             _config.AddItem(new MenuItem("drawtar", "Active Enemy").SetValue(new Circle(true, Color.GreenYellow)));
             _config.AddToMainMenu();
@@ -59,33 +58,6 @@ namespace Rengar
             {
                 if (TargetSelector.GetPriority(target) == 2.5f)
                 {
-                    if (target.IsValidTarget(500))
-                    {
-                        if (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
-                        {
-                            if (_smite != SpellSlot.Unknown)
-                            {
-                                if (ObjectManager.Player.Spellbook.CanUseSpell(_smite) == SpellState.Ready)
-                                {
-                                    ObjectManager.Player.Spellbook.CastSpell(_smite, target);
-                                }
-                            }
-                            if (_smitee != SpellSlot.Unknown)
-                            {
-                                if (ObjectManager.Player.Spellbook.CanUseSpell(_smitee) == SpellState.Ready)
-                                {
-                                    ObjectManager.Player.Spellbook.CastSpell(_smitee, target);
-                                }
-                            }
-                            if (_smiteee != SpellSlot.Unknown)
-                            {
-                                if (ObjectManager.Player.Spellbook.CanUseSpell(_smiteee) == SpellState.Ready)
-                                {
-                                    ObjectManager.Player.Spellbook.CastSpell(_smiteee, target);
-                                }
-                            }
-                        }
-                    }
                     if (target.IsValidTarget(2000))
                     {
                         if (ObjectManager.Player.HasBuff("rengarpassivebuff"))
@@ -100,6 +72,33 @@ namespace Rengar
                                 _config.Item("ForceFocusSelected").SetValue(false);
                             }
                         }
+                        if (target.IsValidTarget(500))
+                        {
+                            if (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
+                            {
+                                if (_smite != SpellSlot.Unknown)
+                                {
+                                    if (ObjectManager.Player.Spellbook.CanUseSpell(_smite) == SpellState.Ready)
+                                    {
+                                        ObjectManager.Player.Spellbook.CastSpell(_smite, target);
+                                    }
+                                }
+                                if (_smitee != SpellSlot.Unknown)
+                                {
+                                    if (ObjectManager.Player.Spellbook.CanUseSpell(_smitee) == SpellState.Ready)
+                                    {
+                                        ObjectManager.Player.Spellbook.CastSpell(_smitee, target);
+                                    }
+                                }
+                                if (_smiteee != SpellSlot.Unknown)
+                                {
+                                    if (ObjectManager.Player.Spellbook.CanUseSpell(_smiteee) == SpellState.Ready)
+                                    {
+                                        ObjectManager.Player.Spellbook.CastSpell(_smiteee, target);
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -107,67 +106,29 @@ namespace Rengar
             {
                 var qwtarget = TargetSelector.GetTarget(_w.Range, TargetSelector.DamageType.Physical);
                 var etarget = TargetSelector.GetTarget(1200, TargetSelector.DamageType.Physical);
-                if (_config.Item("mmode").GetValue<bool>())
-                {
-                    if (!ObjectManager.Player.HasBuff("rengarpassivebuff"))
-                    {
-                        if (qwtarget.IsValidTarget(_w.Range))
-                        {
-                            ObjectManager.Player.IssueOrder(GameObjectOrder.AttackUnit, qwtarget);
-                        }
-                    }
-                }
-                if (!_config.Item("mmode").GetValue<bool>())
-                {
-                    if (!ObjectManager.Player.HasBuff("rengarpassivebuff"))
-                    {
-                        if (qwtarget.IsValidTarget(_w.Range))
-                        {
-                            if (_q.IsReady() || _w.IsReady() || _e.IsReady())
-                            {
-                                ObjectManager.Player.IssueOrder(GameObjectOrder.AttackUnit, qwtarget);
-                            }
-                        }
-                    }
-                }
                 if (ObjectManager.Player.Mana <= 4)
                 {
-                    if (_q.IsReady())
+                    if (qwtarget.IsValidTarget(_w.Range))
                     {
-                        if (qwtarget.IsValidTarget(_q.Range))
+                        _w.Cast();
+                        _e.Cast(qwtarget);
+                    }
+                    if (qwtarget.IsValidTarget(_q.Range))
+                    {
+                        _q.Cast();
+                    }
+                    if (ObjectManager.Player.HasBuff("rengarpassivebuff"))
+                    {
+                        if (etarget.IsValidTarget(1000))
                         {
                             _q.Cast();
                         }
-                        if (ObjectManager.Player.HasBuff("rengarpassivebuff"))
-                        {
-                            if (etarget.IsValidTarget(1000))
-                            {
-                                _q.Cast();
-                            }
-                        }
                     }
-                    if (_w.IsReady())
+                    if (!ObjectManager.Player.HasBuff("rengarpassivebuff"))
                     {
-                        if (qwtarget.IsValidTarget(_w.Range))
+                        if (etarget.IsValidTarget(1000))
                         {
-                            _w.Cast();
-                        }
-                    }
-                    if (_e.IsReady())
-                    {
-                        if (qwtarget.IsValidTarget(_w.Range))
-                        {
-                            _e.Cast(qwtarget);
-                        }
-                        else if (etarget.Distance(ObjectManager.Player.Position) > _w.Range)
-                        {
-                            if (!ObjectManager.Player.HasBuff("rengarpassivebuff"))
-                            {
-                                if (etarget.IsValidTarget(1000))
-                                {
-                                    _e.Cast(etarget);
-                                }
-                            }
+                            _e.Cast(etarget);
                         }
                     }
                 }
@@ -175,39 +136,20 @@ namespace Rengar
                 {
                     if ((ObjectManager.Player.Health/ObjectManager.Player.MaxHealth)*100 > _config.Item("autoheal").GetValue<Slider>().Value)
                     {
-                        if (!_config.Item("emode").GetValue<bool>())
+                        if (_config.Item("ComboMode").GetValue<StringList>().SelectedIndex == 0)
                         {
-                            if (_q.IsReady())
+                            if (qwtarget.IsValidTarget(_q.Range))
                             {
-                                if (qwtarget.IsValidTarget(_q.Range))
+                                _q.Cast();
+                            }
+                            if (ObjectManager.Player.HasBuff("rengarpassivebuff"))
+                            {
+                                if (etarget.IsValidTarget(1000))
                                 {
                                     _q.Cast();
                                 }
-                                if (ObjectManager.Player.HasBuff("rengarpassivebuff"))
-                                {
-                                    if (etarget.IsValidTarget(1000))
-                                    {
-                                        _q.Cast();
-                                    }
-                                }
                             }
-                            if (_e.IsReady())
-                            {
-                                if (etarget.Distance(ObjectManager.Player.Position) > _q.Range)
-                                {
-                                    if (!ObjectManager.Player.HasBuff("rengarpassivebuff"))
-                                    {
-                                        if (etarget.IsValidTarget(1000))
-                                        {
-                                            _e.Cast(etarget);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        if (_config.Item("emode").GetValue<bool>())
-                        {
-                            if (_e.IsReady())
+                            if (etarget.Distance(ObjectManager.Player.Position) > _q.Range)
                             {
                                 if (!ObjectManager.Player.HasBuff("rengarpassivebuff"))
                                 {
@@ -215,6 +157,16 @@ namespace Rengar
                                     {
                                         _e.Cast(etarget);
                                     }
+                                }
+                            }
+                        }
+                        if (_config.Item("ComboMode").GetValue<StringList>().SelectedIndex == 1)
+                        {
+                            if (!ObjectManager.Player.HasBuff("rengarpassivebuff"))
+                            {
+                                if (etarget.IsValidTarget(1000))
+                                {
+                                    _e.Cast(etarget);
                                 }
                             }
                         }
