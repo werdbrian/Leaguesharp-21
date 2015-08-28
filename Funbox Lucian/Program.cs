3dae96b007d51d46c539a70b3f12c45e1262c20c
funboxxx <funboxxx@wp.pl>
2015-08-28 12:28:40 +0100

    1.0.3.6

--- Funbox Lucian/Program.cs
+++ Funbox Lucian/Program.cs
@@ -8,10 +8,16 @@ namespace Lucian
     public class Program
     {
         private static Menu _config;
-        private static Orbwalking.Orbwalker _orbwalker;
-        private static Spell _q, _q2, _w, _w2, _e;
         private static int _lastTick;
-        private static Items.Item _botrk = new Items.Item(3153, 550f), _cutlass = new Items.Item(3144, 550f), _youmuus = new Items.Item(3142, 550f);
+        private static Orbwalking.Orbwalker _orbwalker;
+        private static Spell _q = new Spell(SpellSlot.Q);
+        private static Spell _q2 = new Spell(SpellSlot.Q);
+        private static Spell _w = new Spell(SpellSlot.W);
+        private static Spell _w2 = new Spell(SpellSlot.W);
+        private static Spell _e = new Spell(SpellSlot.E);
+        private static Items.Item _botrk = new Items.Item(3153);
+        private static Items.Item _cutlass = new Items.Item(3144);
+        private static Items.Item _youmuus = new Items.Item(3142);
         private static string[] select = {"Ashe", "Caitlyn", "Corki", "Draven", "Ezreal", "Graves", "Jinx", "Kalista", "KogMaw", "Lucian", "MissFortune","Quinn","Sivir","Teemo","Tristana","TwistedFate","Twitch","Urgot","Varus","Vayne"};
         private static void Main(string[] args)
         {
@@ -23,11 +29,6 @@ namespace Lucian
             {
                 return;
             }
-            _q = new Spell(SpellSlot.Q, 675);
-            _q2 = new Spell(SpellSlot.Q, 1150);
-            _w = new Spell(SpellSlot.W, 700);
-            _w2 = new Spell(SpellSlot.W, 1000);
-            _e = new Spell(SpellSlot.E, 425);
             _q2.SetSkillshot(0.25f, 70, 3000, false, SkillshotType.SkillshotLine);
             _w.SetSkillshot(0.25f, 70, 1500, false, SkillshotType.SkillshotLine);
             _w.MinHitChance = HitChance.Medium;
@@ -48,7 +49,8 @@ namespace Lucian
             _config.SubMenu("Combo").SubMenu("Items").SubMenu("Botrk").AddItem(new MenuItem("Botrk", "Botrk").SetValue(true));
             _config.SubMenu("Combo").SubMenu("Items").SubMenu("Cutlass").AddItem(new MenuItem("Cutlass", "Cutlass").SetValue(true));
             _config.SubMenu("Combo").SubMenu("Items").SubMenu("Youmuus").AddItem(new MenuItem("Youmuus", "Youmuus").SetValue(true));
-            _config.SubMenu("Killsteal").AddItem(new MenuItem("qkil", "Q / Q Extended").SetValue(true));
+            _config.SubMenu("Killsteal").AddItem(new MenuItem("qkil", "Q").SetValue(true));
+            _config.SubMenu("Killsteal").AddItem(new MenuItem("qexkil", "Q Extended").SetValue(true));
             _config.SubMenu("Killsteal").AddItem(new MenuItem("wkil", "W").SetValue(true));
             _config.SubMenu("Harass").AddItem(new MenuItem("qexharhero", "Q Only Certain Champions").SetValue(true));
             foreach (var hero in HeroManager.Enemies) { _config.SubMenu("Harass").SubMenu("Certain Champions").AddItem(new MenuItem("autohar" + hero.ChampionName, hero.ChampionName).SetValue(select.Contains(hero.ChampionName))); }
@@ -83,87 +85,65 @@ namespace Lucian
         private static void Game_OnUpdate(EventArgs args)
         {
             Emode();
+            var ch = HeroManager.Enemies.Where(hero => hero.IsValidTarget(1150)).Where(hero => hero.Distance(ObjectManager.Player) > 675).FirstOrDefault();
+            var thc = HeroManager.Enemies.Where(hero => hero.IsValidTarget(1150)).Where(hero => hero.Distance(ObjectManager.Player) > 675).FirstOrDefault(hero => _config.Item("autohar" + hero.ChampionName).GetValue<bool>());
             var manh = _config.Item("manah").GetValue<Slider>().Value;
-            var manc = _config.Item("manac").GetValue<Slider>().Value;
             var cerha = _config.Item("qexharhero").GetValue<bool>();
-            var cerco = _config.Item("qexcomhero").GetValue<bool>();
-            var qkill = _config.Item("qkil").GetValue<bool>();
-            var wkill = _config.Item("wkil").GetValue<bool>();
-            var useqex = _config.Item("qexcom").GetValue<bool>();
-            foreach (var target in HeroManager.Enemies)
+            if (cerha)
             {
-                var cerh = _config.Item("autohar" + target.ChampionName).GetValue<bool>();
-                var cerc = _config.Item("autocom" + target.ChampionName).GetValue<bool>();
-                if (target != null && target.IsValidTarget(1150))
+                if (_q.IsReady() && (ObjectManager.Player.Mana/ObjectManager.Player.MaxMana)*100 > manh && (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear || _orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed || _orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LastHit))
                 {
-                    if (_q.IsReady())
+                    qexcast(thc);
+                }
+            }
+            else
+            {
+                if (_q.IsReady() && (ObjectManager.Player.Mana/ObjectManager.Player.MaxMana)*100 > manh && (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear || _orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed || _orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LastHit))
+                {
+                    qexcast(ch);
+                }
+            }
+            var tcc = HeroManager.Enemies.Where(hero => hero.IsValidTarget(1150)).Where(hero => hero.Distance(ObjectManager.Player) > 675).FirstOrDefault(hero => _config.Item("autocom" + hero.ChampionName).GetValue<bool>());
+            var manc = _config.Item("manac").GetValue<Slider>().Value;
+            var cerco = _config.Item("qexcomhero").GetValue<bool>();
+            var qc = _config.Item("qexcom").GetValue<bool>();
+            if (qc)
+            {
+                if (cerco)
+                {
+                    if (_q.IsReady() && (ObjectManager.Player.Mana/ObjectManager.Player.MaxMana)*100 > manc && _orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
                     {
-                        //Q Extended logic
-                        if (target.Distance(ObjectManager.Player) > 675)
-                        {
-                            var minions = MinionManager.GetMinions(ObjectManager.Player.ServerPosition, 675, MinionTypes.All, MinionTeam.NotAlly);
-                            foreach (var minion in minions)
-                            {
-                                if (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear || _orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed || _orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LastHit)
-                                {
-                                    if ((ObjectManager.Player.Mana/ObjectManager.Player.MaxMana)*100 > manh)
-                                    {
-                                        //laneclear, mixed, lasthit certain champions + check mana
-                                        if (cerh && _q2.WillHit(target, ObjectManager.Player.ServerPosition.Extend(minion.ServerPosition, 1150), 0, HitChance.VeryHigh))
-                                        {
-                                            _q2.CastOnUnit(minion);
-                                        }
-                                        //laneclear, mixed, lasthit all champions + check mana
-                                        if (!cerha && _q2.WillHit(target, ObjectManager.Player.ServerPosition.Extend(minion.ServerPosition, 1150), 0, HitChance.VeryHigh))
-                                        {
-                                            _q2.CastOnUnit(minion);
-                                        }
-                                    }
-                                }
-                                if (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
-                                {
-                                    if ((ObjectManager.Player.Mana/ObjectManager.Player.MaxMana)*100 > manc)
-                                    {
-                                        //combo certain champions + check mana
-                                        if (useqex && cerc && _q2.WillHit(target, ObjectManager.Player.ServerPosition.Extend(minion.ServerPosition, 1150), 0, HitChance.VeryHigh))
-                                        {
-                                            _q2.CastOnUnit(minion);
-                                        }
-                                        //combo all champions + check mana
-                                        if (useqex && !cerco && _q2.WillHit(target, ObjectManager.Player.ServerPosition.Extend(minion.ServerPosition, 1150), 0, HitChance.VeryHigh))
-                                        {
-                                            _q2.CastOnUnit(minion);
-                                        }
-                                    }
-                                }
-                                //Q Extended killsteal
-                                if (ObjectManager.Player.GetSpellDamage(target, SpellSlot.Q) > target.Health && qkill && _q2.WillHit(target, ObjectManager.Player.ServerPosition.Extend(minion.ServerPosition, 1150), 0, HitChance.VeryHigh))
-                                {
-                                    _q2.CastOnUnit(minion);
-                                }
-                            }
-                        }
-                        //Q killsteal
-                        if (target.Distance(ObjectManager.Player) <= 675)
-                        {
-                            if (qkill && ObjectManager.Player.GetSpellDamage(target, SpellSlot.Q) > target.Health)
-                            {
-                                _q.CastOnUnit(target);
-                            }
-                        }
+                        qexcast(tcc);
                     }
-                    //W killsteal
-                    if (_w.IsReady() && target.Distance(ObjectManager.Player) <= 1000)
+                }
+                else
+                {
+                    if (_q.IsReady() && (ObjectManager.Player.Mana/ObjectManager.Player.MaxMana)*100 > manc && _orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
                     {
-                        if (wkill && ObjectManager.Player.GetSpellDamage(target, SpellSlot.W) > target.Health)
-                        {
-                            var WPred = _w2.GetPrediction(target);
-                            if (WPred.Hitchance >= HitChance.High)
-                            {
-                                _w2.Cast(WPred.CastPosition);
-                            }
-                        }
+                        qexcast(ch);
                     }
+                }
+            }
+            var qk = _config.Item("qkil").GetValue<bool>();
+            var qke = _config.Item("qexkil").GetValue<bool>();
+            var kqe = HeroManager.Enemies.Where(hero => hero.IsValidTarget(1150)).Where(hero => hero.Distance(ObjectManager.Player) > 675).FirstOrDefault(hero => ObjectManager.Player.GetSpellDamage(hero, SpellSlot.Q) >= hero.Health);
+            var kq = HeroManager.Enemies.Where(hero => hero.IsValidTarget(675)).FirstOrDefault(hero => ObjectManager.Player.GetSpellDamage(hero, SpellSlot.Q) >= hero.Health);
+            if (qk && _q.IsReady())
+            {
+                _q.CastOnUnit(kq);
+            }
+            if (qke && _q.IsReady())
+            {
+                qexcast(kqe);
+            }
+            var wk = _config.Item("wkil").GetValue<bool>();
+            var kw = HeroManager.Enemies.Where(hero => hero.IsValidTarget(1000)).FirstOrDefault(hero => ObjectManager.Player.GetSpellDamage(hero, SpellSlot.W) >= hero.Health);
+            if (wk && _w.IsReady())
+            {
+                var WPred = _w2.GetPrediction(kw);
+                if (WPred.Hitchance >= HitChance.High)
+                {
+                    _w2.Cast(WPred.CastPosition);
                 }
             }
         }
@@ -178,28 +158,30 @@ namespace Lucian
             var youuse = _config.Item("Youmuus").GetValue<bool>();
             var EMode =_config.Item("emod").GetValue<StringList>().SelectedIndex;
             var pos = Geometry.CircleCircleIntersection(ObjectManager.Player.ServerPosition.To2D(), Prediction.GetPrediction(obj, 0.25f).UnitPosition.To2D(), 425, Orbwalking.GetRealAutoAttackRange(obj));
-            if (enemy != null && enemy.IsValidTarget())
+            if (enemy.IsValidTarget())
             {
                 if (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed)
                 {
-                    //Q Usage
-                    if (_q.IsReady() && quse)
+                    if (quse)
                     {
-                        _q.CastOnUnit(enemy);
-                    }
-                    //W Usage
-                    if (!_q.IsReady() && _w.IsReady() && wuse)
-                    {
-                        _w.Cast(enemy);
+                        if (_q.IsReady())
+                        {
+                            _q.CastOnUnit(enemy);
+                        }
+                        else
+                        {
+                            if (_w.IsReady() && wuse)
+                            {
+                                Utility.DelayAction.Add(200, Wuse);
+                            }
+                        }
                     }
                 }
                 if (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
                 {
                     switch (EMode)
                     {
-                        //E Safe
                         case 0:
-                            //E Usage
                             if (_e.IsReady())
                             {
                                 if (pos.Count() > 0)
@@ -211,52 +193,57 @@ namespace Lucian
                                     _e.Cast(ObjectManager.Player.ServerPosition.Extend(obj.ServerPosition, -425));
                                 }
                             }
-                            //Q Usage
-                            if (!_e.IsReady() && _q.IsReady() && quse)
+                            else
                             {
-                                _q.CastOnUnit(enemy);
-                            }
-                            //W Usage
-                            if (!_q.IsReady() && !_e.IsReady() && _w.IsReady() && wuse)
-                            {
-                                _w.Cast(enemy);
+                                if (_q.IsReady() && quse)
+                                {
+                                    _q.CastOnUnit(enemy);
+                                }
+                                else
+                                {
+                                    if (_w.IsReady() && wuse)
+                                    {
+                                        Utility.DelayAction.Add(200, Wuse);
+                                    }
+                                }
                             }
                         break;
-                        //E To Mouse
                         case 1:
-                            //E Usage
                             if (_e.IsReady())
                             {
                                 _e.Cast(ObjectManager.Player.ServerPosition.Extend(Game.CursorPos, 425));
                             }
-                            //Q Usage
-                            if (!_e.IsReady() && _q.IsReady() && quse)
+                            else
                             {
-                                _q.CastOnUnit(enemy);
-                            }
-                            //W Usage
-                            if (!_q.IsReady() && !_e.IsReady() && _w.IsReady() && wuse)
-                            {
-                                _w.Cast(enemy);
+                                if (_q.IsReady() && quse)
+                                {
+                                    _q.CastOnUnit(enemy);
+                                }
+                                else
+                                {
+                                    if (_w.IsReady() && wuse)
+                                    {
+                                        Utility.DelayAction.Add(200, Wuse);
+                                    }
+                                }
                             }
                         break;
-                        //E None
                         case 2:
-                            //Q Usage
                             if (_q.IsReady() && quse)
                             {
                                 _q.CastOnUnit(enemy);
                             }
-                            //W Usage
-                            if (!_q.IsReady() && _w.IsReady() && wuse)
+                            else
                             {
-                                _w.Cast(enemy);
+                                if (_w.IsReady() && wuse)
+                                {
+                                    Utility.DelayAction.Add(200, Wuse);
+                                }
                             }
                         break;
                     }
                     if (enemy.Distance(ObjectManager.Player) < 550)
                     {
-                        //ITEMS
                         if (_botrk.IsReady() && botuse)
                         {
                             _botrk.Cast(enemy);
@@ -285,7 +272,6 @@ namespace Lucian
             var Eran = _config.Item("ERange").GetValue<Circle>();
             var Eaaran = _config.Item("EaaRange").GetValue<Circle>();
             var Rran = _config.Item("RRange").GetValue<Circle>();
-            //Draw Target
             if (drt.Active)
             {
                 var td = TargetSelector.GetTarget(700, TargetSelector.DamageType.Physical);
@@ -294,7 +280,6 @@ namespace Lucian
                     Render.Circle.DrawCircle(td.Position, 115f, drt.Color, 1);
                 }
             }
-            //Draw E Mode
             if (emdraw)
             {
                 switch (EMode)
@@ -310,7 +295,6 @@ namespace Lucian
                     break;
                 }
             }
-            //Spells Range
             if (Qran.Active)
             {
                 Render.Circle.DrawCircle(ObjectManager.Player.Position, 675, Qran.Color);
@@ -336,7 +320,6 @@ namespace Lucian
                 Render.Circle.DrawCircle(ObjectManager.Player.Position, 1400, Rran.Color);
             }
         }
-        //E Mode Switch
         private static void Emode()
         {
             var lasttime = Environment.TickCount - _lastTick;
@@ -360,6 +343,22 @@ namespace Lucian
                     _lastTick = Environment.TickCount + 300;
                 break;
             }
+        }
+        private static void qexcast(Obj_AI_Hero unit)
+        {
+            var minions = MinionManager.GetMinions(ObjectManager.Player.ServerPosition, 675, MinionTypes.All, MinionTeam.NotAlly);
+            foreach (var minion in minions)
+            {
+                if (_q2.WillHit(unit, ObjectManager.Player.ServerPosition.Extend(minion.ServerPosition, 1150), 0, HitChance.VeryHigh))
+                {
+                    _q2.CastOnUnit(minion);
+                }
+            }
+        }
+        private static void Wuse()
+        {
+            var t = TargetSelector.GetTarget(700, TargetSelector.DamageType.Physical);
+            _w.Cast(t);
         }
     }
 }
